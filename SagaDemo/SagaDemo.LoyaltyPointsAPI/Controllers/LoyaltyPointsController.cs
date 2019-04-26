f1702dab428e95c98fce2ff5fab1c2d7d4be1ff0
BalazsArva <balazs.arva@windowslive.com>
2019-04-26 13:54:53 +0200

    Created command handler for consuming loyalty points.

--- SagaDemo/SagaDemo.LoyaltyPointsAPI/Controllers/LoyaltyPointsController.cs
+++ SagaDemo/SagaDemo.LoyaltyPointsAPI/Controllers/LoyaltyPointsController.cs
@@ -12,16 +12,28 @@ namespace SagaDemo.LoyaltyPointsAPI.Controllers
     public class LoyaltyPointsController : ControllerBase
     {
         private readonly ICommandHandler<EarnPointsCommand> earnPointsCommandHandler;
+        private readonly ICommandHandler<ConsumePointsCommand> consumePointsCommandHandler;
 
-        public LoyaltyPointsController(ICommandHandler<EarnPointsCommand> earnPointsCommandHandler)
+        public LoyaltyPointsController(
+            ICommandHandler<EarnPointsCommand> earnPointsCommandHandler,
+            ICommandHandler<ConsumePointsCommand> consumePointsCommandHandler)
         {
             this.earnPointsCommandHandler = earnPointsCommandHandler ?? throw new ArgumentNullException(nameof(earnPointsCommandHandler));
+            this.consumePointsCommandHandler = consumePointsCommandHandler ?? throw new ArgumentNullException(nameof(consumePointsCommandHandler));
         }
 
         [HttpPost("earn")]
         public async Task<IActionResult> EarnPoints(EarnPointsCommand command, CancellationToken cancellationToken)
         {
             await earnPointsCommandHandler.HandleAsync(command, cancellationToken).ConfigureAwait(false);
+
+            return NoContent();
+        }
+
+        [HttpPost("consume")]
+        public async Task<IActionResult> ConsumePoints(ConsumePointsCommand command, CancellationToken cancellationToken)
+        {
+            await consumePointsCommandHandler.HandleAsync(command, cancellationToken).ConfigureAwait(false);
 
             return NoContent();
         }
---
+++ SagaDemo/SagaDemo.LoyaltyPointsAPI/Handlers/CommandHandlers/ConsumePointsCommandHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SagaDemo.LoyaltyPointsAPI.DataAccess;
+using SagaDemo.LoyaltyPointsAPI.DataAccess.Entities;
+using SagaDemo.LoyaltyPointsAPI.Operations.Commands;
+
+namespace SagaDemo.LoyaltyPointsAPI.Handlers.CommandHandlers
+{
+    public class ConsumePointsCommandHandler : ICommandHandler<ConsumePointsCommand>
+    {
+        private const string ConsumePointsReason = "ConsumePoints";
+
+        private readonly ILoyaltyDbContextFactory dbContextFactory;
+
+        public ConsumePointsCommandHandler(ILoyaltyDbContextFactory dbContextFactory)
+        {
+            this.dbContextFactory = dbContextFactory ?? throw new ArgumentNullException(nameof(dbContextFactory));
+        }
+
+        public async Task HandleAsync(ConsumePointsCommand command, CancellationToken cancellationToken)
+        {
+            using (var context = dbContextFactory.CreateDbContext())
+            {
+                var totalPoints = await context.PointsChangedEvents.SumAsync(evt => evt.PointChange).ConfigureAwait(false);
+
+                if (totalPoints < command.Points)
+                {
+                    // TODO: Create and throw custom error
+                }
+
+                context.PointsChangedEvents.Add(new PointsChangedEvent
+                {
+                    PointChange = -command.Points,
+                    Reason = ConsumePointsReason,
+                    UtcDateTimeRecorded = DateTime.UtcNow,
+                    UserId = command.UserId
+                });
+
+                // Assume optimistic concurrency so that points aren't changed between retrieving the sum and adding the consume record.
+                await context.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+            }
+        }
+    }
+}
