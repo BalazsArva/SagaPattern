9006d39156482e4f47d52932a80360e520fc2d2e
Árva Balázs <balazs.arva@windowslive.com>
2019-04-27 13:25:32 +0200

    API endpoint skeletons for inventory.

---
+++ SagaDemo/SagaDemo.InventoryAPI/Controllers/CatalogController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SagaDemo.InventoryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> CreateItem(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetItem(int id, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("{id}/reservations")]
+        public async Task<IActionResult> ReserveItem(int id, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("{id}/takeout")]
+        public async Task<IActionResult> TakeoutItem(int id, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("{id}/bringback")]
+        public async Task<IActionResult> BringbackItem(int id, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
